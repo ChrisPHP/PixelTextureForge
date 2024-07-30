4d8064eaec10601ac026e9d01e2e4c7167369033
ChrisPHP <chris.page57@ntlworld.com>
2024-07-30 20:59:03 +0100

    Improve mortar noise edges

--- src/procedural_textures/brick.py
+++ src/procedural_textures/brick.py
@@ -40,6 +40,17 @@ def random_brick_colour(base_colour, variation=10):
 
 
 def create_brick_texture(width, height, noise, brick_width, brick_height, mortar_size):
+    def get_mortar_gradient(size, img_array):
+        kernel_size = round(size / 2)
+        open_cv_image = img_array[:, :, ::-1].copy()
+    
+        edges = cv2.Canny(open_cv_image, 100, 200)
+        kernel = np.ones((kernel_size,kernel_size), np.uint8)
+        dilated_edges = cv2.dilate(edges, kernel, iterations=2)
+        distance = cv2.distanceTransform(255 - dilated_edges, cv2.DIST_L2, 5)
+        gradient = np.clip(1 - distance / size, 0, 1)
+        return gradient
+
     mask_brick_colour = (0, 0, 0, 0)
     mask_mortar_colour = (255, 255, 255, 255)
     base_brick_colour = (180, 80, 80, 255)
@@ -55,9 +66,12 @@ def create_brick_texture(width, height, noise, brick_width, brick_height, mortar
     num_bricks_x = width // (brick_width + mortar_size)
     num_bricks_y = height // (brick_height + mortar_size)
 
+    #Generate brick textures
+    edge_brick_colour = base_brick_colour
     for y in range(num_bricks_y):
         for x in range(num_bricks_x):
             offset = (brick_width + mortar_size) // 2 if y % 2 == 1 else 0
+            mortar_edge = round(mortar_size / 2)
 
             left = x * (brick_width + mortar_size) - offset
             top = y * (brick_height + mortar_size)
@@ -65,43 +79,50 @@ def create_brick_texture(width, height, noise, brick_width, brick_height, mortar
             bottom = top + brick_height
 
             brick_colour = random_brick_colour(base_brick_colour)
-            draw.rectangle([left, top, right, bottom], fill=mask_brick_colour)
             draw_colour.rectangle([left, top, right, bottom], fill=brick_colour)
+            if x == 0:
+                edge_brick_colour = brick_colour
+                if y == 0:
+                    draw.rectangle([left+mortar_edge, top+mortar_edge, right, bottom], fill=mask_brick_colour)
+                elif offset == 0:
+                    draw.rectangle([left+mortar_edge, top, right, bottom], fill=mask_brick_colour)
+                else:
+                    draw.rectangle([left, top, right, bottom], fill=mask_brick_colour)
+            elif y==0:
+                draw.rectangle([left+mortar_edge, top+mortar_edge, right, bottom], fill=mask_brick_colour)
+            else:
+                draw.rectangle([left, top, right, bottom], fill=mask_brick_colour)
             if y % 2 and x == num_bricks_x-1:
                 draw.rectangle([right+mortar_size, top, width, bottom], fill=mask_brick_colour)
-                draw_colour.rectangle([right+mortar_size, top, width, bottom], fill=brick_colour)
+                draw_colour.rectangle([right+mortar_size, top, width, bottom], fill=edge_brick_colour)
 
     brick_colour_array = np.array(img_colour)
     brick_array = np.array(img)
-    open_cv_image = brick_array[:, :, ::-1].copy()
-    
-    edges = cv2.Canny(open_cv_image, 100, 200)
-    kernel = np.ones((2,2), np.uint8)
-    dilated_edges = cv2.dilate(edges, kernel, iterations=2)
-    distance = cv2.distanceTransform(255 - dilated_edges, cv2.DIST_L2, 5)
-    gradient = np.clip(1 - distance / 10, 0, 1)
+    gradient = get_mortar_gradient(mortar_size, brick_array)
 
+    #Shape noise array to match image size
     noise_height, noise_width = noise.shape
     tiled_noise = np.tile(noise, (height // noise_height + 1, width // noise_width + 1))
     tiled_noise = tiled_noise[:height, :width]
 
+    #Create mask for the mortar noise gradient
     edge_noise = np.copy(tiled_noise)
     edge_noise = edge_noise * gradient
     alpha_mask = (edge_noise > 0.2).astype(float)
 
+    #Apply noise to image
     mask_with_noise = brick_array.copy()
-
-
     for i in range(3):
         brick_colour_array[:,:,i] = np.clip(brick_colour_array[:,:,i].astype(np.float32) + tiled_noise * 30, 0, 255).astype(np.uint8)
         mask_with_noise[:,:,i] = np.clip(mask_with_noise[:,:,i].astype(np.float32) + alpha_mask * 200, 0, 255).astype(np.uint8)
 
+    #Composite images together
     color_coverted = cv2.cvtColor(mask_with_noise, cv2.COLOR_BGR2RGBA) 
-    
     img = Image.fromarray(color_coverted.astype('uint8'))
     old_mask = Image.fromarray(brick_array.astype('uint8'))
     comp = Image.composite(old_mask, img, old_mask)
 
+    #Convert
     d = comp.getdata()
     new_comp = []
     for item in d:
